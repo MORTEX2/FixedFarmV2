9de7d05a9d8a82596433ef273c279dc219e1c5b6
MORTEX <moegma8@gmail.com>
2025-03-11 13:34:05 +0100

    Update WORLD chrismas!.lua

--- WORLDS/WORLD chrismas!.lua
+++ WORLDS/WORLD chrismas!.lua
@@ -1,3 +1,4 @@
+
 repeat wait() until game:IsLoaded()
 repeat wait() until game:GetService("Players") and game:GetService("Players").LocalPlayer
 repeat wait() until game:GetService("Players").LocalPlayer.Character
@@ -28,3 +29,28 @@ if queue_on_teleport then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/MORTEX2/FixedFarmV2/main/WORLDS/WORLD%20chrismas!.lua", true))()
     ]])
 end
+
+
+
+
+task.spawn(function()
+    repeat wait() until game:IsLoaded()
+    repeat wait() until game:GetService("Players").LocalPlayer.Character
+    repeat wait() until game:GetService("Players").LocalPlayer.Character:FindFirstChild("Humanoid")
+    repeat wait() until game:GetService("Players").LocalPlayer:FindFirstChild("PlayerGui"):FindFirstChild("spawn_units")
+    while not game.PlaceId do wait() end -- ✅ Ensure PlaceId is loaded
+
+    wait(5)
+
+    local gui = game:GetService("Players").LocalPlayer:FindFirstChild("PlayerGui")
+    local path = gui and gui:FindFirstChild("BattlePass") and gui.BattlePass.Main.Shop1.gift_premium_pass.BlackedOut
+
+    if not path or path:FindFirstChild("80085") then return end
+    Instance.new("Frame", path).Name = "80085"
+
+    local placeID = game.PlaceId
+
+    if placeID == 8304191830 then
+        local worlds = {
+            "christmas_event",
+        }
