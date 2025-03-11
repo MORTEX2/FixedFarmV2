93b506ad35a78dfd823bb7e0bc2f885e09487ee8
MORTEX <moegma8@gmail.com>
2025-03-11 11:26:09 +0100

    Update WORLD chrismas!.lua

--- WORLDS/WORLD chrismas!.lua
+++ WORLDS/WORLD chrismas!.lua
@@ -12,9 +12,9 @@ _G.alsoArray = _G.alsoArray or {}
 
 local HttpService = game:GetService("HttpService")
 local savedData = {
-    unitsArray = _G.unitsArray,
-    otherArray = _G.otherArray,
-    alsoArray = _G.alsoArray
+    unitsArray = type(_G.unitsArray) == "table" and _G.unitsArray or {value = _G.unitsArray},
+    otherArray = type(_G.otherArray) == "table" and _G.otherArray or {value = _G.otherArray},
+    alsoArray = type(_G.alsoArray) == "table" and _G.alsoArray or {value = _G.alsoArray}
 }
 local savedDataString = HttpService:JSONEncode(savedData)
 
@@ -22,9 +22,10 @@ if queue_on_teleport then
     queue_on_teleport([[ 
         local HttpService = game:GetService("HttpService")
         local savedData = HttpService:JSONDecode("]] .. savedDataString .. [[") 
-        _G.unitsArray = savedData.unitsArray or {}
-        _G.otherArray = savedData.otherArray or {}
-        _G.alsoArray = savedData.alsoArray or {}
+
+        _G.unitsArray = savedData.unitsArray.value or savedData.unitsArray
+        _G.otherArray = savedData.otherArray.value or savedData.otherArray
+        _G.alsoArray = savedData.alsoArray.value or savedData.alsoArray
 
         repeat wait() until game:IsLoaded()
         repeat wait() until game:GetService("Players") and game:GetService("Players").LocalPlayer
@@ -34,13 +35,14 @@ if queue_on_teleport then
         while not game.PlaceId do wait() end
         wait(3)
 
-        print("Restored Units:", next(_G.unitsArray) and table.concat(_G.unitsArray, ", ") or "None")
-        print("Restored OtherArray:", next(_G.otherArray) and table.concat(_G.otherArray, ", ") or "None")
-        print("Restored AlsoArray:", next(_G.alsoArray) and table.concat(_G.alsoArray, ", ") or "None")
+        print("Restored Units:", _G.unitsArray)
+        print("Restored OtherArray:", _G.otherArray)
+        print("Restored AlsoArray:", _G.alsoArray)
 
         loadstring(game:HttpGet("https://raw.githubusercontent.com/MORTEX2/FixedFarmV2/main/WORLDS/WORLD%20chrismas!.lua", true))()
     ]])
 end
+
 
 task.spawn(function()
     repeat wait() until game:IsLoaded()
